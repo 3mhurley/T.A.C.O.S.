a21357f5cc02ccd9bfbd5d17cadc186e4a0f6201
Anne Wilson <annewilson2591@gmail.com>
2019-02-12 18:56:06 -0700

    AW update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,79 +1,79 @@
-// Object
-var taco = {
-    map: 'taco.com',
-    foursquare_clientID: 'D0HCALZZRSU3MQSBUY0E1AS4PKXFOIPTIO5UP11JMKEN3YSJ',
-    foursquare_clientSecret: 'PGOTQALEY5RLYXXTV0XRG3WOU3MFT52BXJLPUC232LONJ3FM',
-}
+// // Object
+// var taco = {
+//     map: 'taco.com',
+//     foursquare_clientID: 'D0HCALZZRSU3MQSBUY0E1AS4PKXFOIPTIO5UP11JMKEN3YSJ',
+//     foursquare_clientSecret: 'PGOTQALEY5RLYXXTV0XRG3WOU3MFT52BXJLPUC232LONJ3FM',
+// }
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-"https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-var mapURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap&region=US"
+// "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+// var mapURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap&region=US"
 
-function initMap() {
-    var map = new google.maps.Map(mapURL, {
-        zoom: 8,
-        center: {lat: 35.717, lng: 139.731}
-    });
-}
+// function initMap() {
+//     var map = new google.maps.Map(mapURL, {
+//         zoom: 8,
+//         center: {lat: 35.717, lng: 139.731}
+//     });
+// }
 
-// Aye, Jacks! 4 Square ahead! Yar
-var city = 'near=' + 'Denver, CO';
-var address = '1701 WYNKOOP DENVER, CO 80202' // Union
-var location;
-var query = 'query=' + 'tacos'
-var queryURL = 'https://api.foursquare.com/v2/venues/search?' + 'client_id=' + taco.foursquare_clientID + '&client_secret=' + taco.foursquare_clientSecret + '&intent=checkin' + '&' + query + '&' + city + '&radius=50' + '&limit=10';
-console.log(queryURL);
+// // Aye, Jacks! 4 Square ahead! Yar
+// var city = 'near=' + 'Denver, CO';
+// var address = '1701 WYNKOOP DENVER, CO 80202' // Union
+// var location;
+// var query = 'query=' + 'tacos'
+// var queryURL = 'https://api.foursquare.com/v2/venues/search?' + 'client_id=' + taco.foursquare_clientID + '&client_secret=' + taco.foursquare_clientSecret + '&intent=checkin' + '&' + query + '&' + city + '&radius=50' + '&limit=10';
+// console.log(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response) {
-    console.log(response);
+// $.ajax({
+//     url: queryURL,
+//     method: "GET",
+// }).then(function(response) {
+//     console.log(response);
 
-});
+// });
 
-});
+// });
 
-// Google Map with Geolocation
-// -Declare vars and initialize function
-var map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644}, // Sydney
-        zoom: 12 // default zoom to local area <5mi
-    });
-infoWindow = new google.maps.InfoWindow;
+// // Google Map with Geolocation
+// // -Declare vars and initialize function
+// var map, infoWindow;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644}, // Sydney
+//         zoom: 12 // default zoom to local area <5mi
+//     });
+// infoWindow = new google.maps.InfoWindow;
 
-// -Geocode current position
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
+// // -Geocode current position
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//         var pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude
+//         };
 
-        infoWindow.setPosition(pos); // set position of infoWindow on geocode
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos); // center map
-    }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
-    } else {
-        // --If browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
-// -Error handling
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
-<script src="https://www.gstatic.com/firebasejs/5.8.2/firebase.js"></script>
+//         infoWindow.setPosition(pos); // set position of infoWindow on geocode
+//         infoWindow.setContent('Location found.');
+//         infoWindow.open(map);
+//         map.setCenter(pos); // center map
+//     }, function() {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//     } else {
+//         // --If browser doesn't support Geolocation
+//         handleLocationError(false, infoWindow, map.getCenter());
+//     }
+// }
+// // -Error handling
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent(browserHasGeolocation ?
+//                           'Error: The Geolocation service failed.' :
+//                           'Error: Your browser doesn\'t support geolocation.');
+//     infoWindow.open(map);
+// }
+
 
   // Initialize Firebase
   var config = {
@@ -88,7 +88,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
   var dataRef = firebase.database();
 
-
   //initial values 
   var locationInput = "";
   var distanceInput = 0;
@@ -97,7 +96,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   //onclick event of submit button on page 1 and 2
   $("#hugeButton").on("click", function(event) {
     event.preventDefault(); 
-
 
     //grabbing user input
     locationInput = $("#location").val().trim();
@@ -108,27 +106,27 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     console.log(distanceInput);
     console.log(priceInput);
 
-    //clears elements
-    $("#location").val(""),
-    $("#distance").val(""),
-    $("#price").val("");
+    // //clears elements
+    // $("#location").val(""),
+    // $("#distance").val(""),
+    // $("#price").val("");
 
     //taco location info for firebase
     var newTaco = {
-      locationInput: locationInput,
-      distanceInput: distanceInput,
-      priceInput: priceInput,
+      fireLocation: locationInput,
+      fireDistance: distanceInput,
+      firePrice: priceInput,
     };
 
     //push to firebase
     dataRef.ref().push(newTaco);
 
-    //add AJAX call
+    //add AJAX call function
 
     //redirect to page 2
+    $(location).attr('href', 'page2a.html');
+    //window.location.assign("page2a.html");
     //window.location.href = "page2a.html"; 
-    //$(location).attr('href', 'page2a.html');
-    window.location.assign("page2a.html");
   
 
   //end onclick
@@ -140,9 +138,9 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
     //no user input necessary 
 
-    //add AJAX call
+    //add AJAX call function
 
-    //redirect to page 2
+    //redirect to page 2 
     //window.location.href = "/page2a.html";
 
   //end onclick
@@ -157,7 +155,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
     //output of result - into results table 
     //need to specify results parameters as variables 
-    $("#resultsTable").append("<tr><td>" + locationInput + "</td><td>" + distanceInput + "</td><td>" + priceInput + "</td></tr>");
+    $("#resultsTable").append("<tr><td>" + fireLocation + "</td><td>" + fireDistance + "</td><td>" + firePrice + "</td></tr>");
 
 
   });
@@ -166,28 +164,16 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
   //pre-populate page 1 user input into page 2 user input
   //NOT FUNCTIONAL 
-  $(document).ready(function(autofill) {
-    $("hugeButton").click(function() {
-      $("#location").val().trim();
-    });
-  });
+  // $(document).ready(function(autofill) {
+  //   $("hugeButton").click(function() {
+  //     $("#location").val().trim();
+  //   });
+  // });
 
   //placeholder value -pre-populate with input 
   
-    //output of random result - into results table 
+  //output of random result - into results table 
     
-
-
-
-  //page 2
   //search results in nav bar 
   
-  //pre-populate with input 
-  //search button 
-
-  //back from request 
-  //fill the map (replace the anchor or image)
-  //store the response in the card
-  //dynamically append with additional data 
-
-  //on click card - dynamically create the nav bar 
+  
