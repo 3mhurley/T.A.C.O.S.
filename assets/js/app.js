c1260a5aeb767fcf59585b57b69c490727571b0a
EH <33641969+3mhurley@users.noreply.github.com>
2019-02-14 21:36:32 -0700

    many adds

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,87 +3,129 @@ var taco = {
     map: 'taco.com',
     foursquare_clientID: 'D0HCALZZRSU3MQSBUY0E1AS4PKXFOIPTIO5UP11JMKEN3YSJ',
     foursquare_clientSecret: 'PGOTQALEY5RLYXXTV0XRG3WOU3MFT52BXJLPUC232LONJ3FM',
+    gcp_jma_key: 'AIzaSyAfQ3fFasbNNaSb6Y8SSK16iDsh4_rvvRU',
+    pos: [],
 }
+
+// LOCATION LOCATION LOCATION
+function getLocation(callback) {
+  var promise = new Promise(function(resolve, reject) {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+              function(position){
+                  resolve("@" + position.coords.latitude + "," + position.coords.longitude)
+              }
+          );
+      } else {
+        reject("Unknown");
+      }
+  });
+
+  return promise;
+}
+
+
+
+// // -Declare vars and initialize function
+// var mapURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAfQ3fFasbNNaSb6Y8SSK16iDsh4_rvvRU&callback=initMap"
+// var map, infoWindow, pos;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: -34.397, lng: 150.644}, // Sydney
+//         zoom: 12 // default zoom to local area <5mi
+//     });
+// infoWindow = new google.maps.InfoWindow;
+
+// // -Geocode current position
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//         pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude
+//         };
+//         infoWindow.setPosition(pos); // set position of infoWindow on geocode
+//         infoWindow.setContent('Location found.');
+//         infoWindow.open(map);
+//         map.setCenter(pos); // center map
+//     }, function() {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//     } else {
+//         // --If browser doesn't support Geolocation
+//         handleLocationError(false, infoWindow, map.getCenter());
+//         console.log("SAY YES!");
+//     }
+// }
+// // -Error handling
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//     infoWindow.setPosition(pos);
+//     infoWindow.setContent(browserHasGeolocation ?
+//                           'Error: The Geolocation service failed.' :
+//                           'Error: Your browser doesn\'t support geolocation.');
+//     infoWindow.open(map);
+// }
 
 $(document).ready(function() {
 
-"https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-var mapURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap&region=US"
-
-function initMap() {
-    var map = new google.maps.Map(mapURL, {
-        zoom: 8,
-        center: {lat: 35.717, lng: 139.731}
-    });
-}
-
 // Aye, Jacks! 4 Square ahead! Yar
-var city = 'Denver, CO' //&near=Denver, CO
-var address = '1701 WYNKOOP DENVER, CO 80202' // Union
-var now = '20190212' //&v=YYYYMMDD
-var location = '39.7,105.0' //&ll=40.7,-74
-var query = 'tacos'
-var queryURL = 'https://api.foursquare.com/v2/venues/search?'
+var city = 'Denver, CO'; //&near=Denver, CO
+var address = '1701 WYNKOOP DENVER, CO 80202'; // Union
+var now = '20190212'; //&v=YYYYMMDD
+var location = '39.7,105.0'; //&ll=40.7,-74
+var query = 'tacos';
+var price = '1,2,3,4';
+var queryURL = 'https://api.foursquare.com/v2/venues/explore?'
     // + 'll=' + location;
     + 'near=' + city
-    + '&intent=' + 'checkin'
-    + '&radius=' + '50'
+    + '&radius=' + '5000'
+    // + '&section=' + 'Taco Place' // Taco Place
     + '&query=' + query
     + '&limit=' + '10'
+    + '&openNow=' + '1'
+    + '&sortByDistance=' + '1'
+    + '&price=' + price
     + '&client_id=' + taco.foursquare_clientID
     + '&client_secret=' + taco.foursquare_clientSecret
     + '&v=' + now;
 console.log(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response) {
-    console.log(response);
-
-});
-
-});
-
-// Google Map with Geolocation
-// -Declare vars and initialize function
-var map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644}, // Sydney
-        zoom: 12 // default zoom to local area <5mi
-    });
-infoWindow = new google.maps.InfoWindow;
-
-// -Geocode current position
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos); // set position of infoWindow on geocode
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos); // center map
-    }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-    });
-    } else {
-        // --If browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
-// -Error handling
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+function getList(val) {
+  var queryURL = val
+  $.ajax({
+      url: queryURL,
+      method: "GET",
+  }).then(function(rsp) {
+      console.log(rsp.response);
+      console.log(rsp.response.groups[0].items[0].venue.id);
+      getDeets(rsp.response.groups[0].items[0].venue.id);
+  });
 }
 
+function getDeets(val) {
+
+  var venueID = val;
+  var queryURL = 'https://api.foursquare.com/v2/venues/'
+    + venueID + '?'
+    + '&client_id=' + taco.foursquare_clientID
+    + '&client_secret=' + taco.foursquare_clientSecret
+    + '&v=' + now;
+  $.ajax({
+      url: queryURL,
+      method: "GET",
+  }).then(function(rsp) {
+      console.log(rsp.response.venue);
+      console.log(rsp.response.venue.name);
+      console.log(rsp.response.venue.attributes.groups[0].summary);
+      console.log(rsp.response.venue.contact.formattedPhone);
+      console.log(rsp.response.venue.url);
+      console.log(rsp.response.venue.rating);
+      console.log(rsp.response.venue.description);
+  });
+}
+
+getList(queryURL);
+
+}); // Close document.ready
 
 
   // Initialize Firebase
